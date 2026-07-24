7c7e52acc163b9e3b07c0b2c34f28e860e3460a9
Raijin <the_krap7@mail.ru>
2026-07-24 23:41:55 +0500

    Drop double plants with any tool
    Library-side semantic port of https://github.com/quiqueck/BetterEnd/commit/de09e330

--- src/main/java/org/betterx/bclib/blocks/BaseDoublePlantBlock.java
+++ src/main/java/org/betterx/bclib/blocks/BaseDoublePlantBlock.java
@@ -157,7 +157,7 @@ public abstract class BaseDoublePlantBlock extends BaseBlockNotFull implements R
             @NotNull LootLookupProvider provider,
             @NotNull ResourceKey<LootTable> tableKey
     ) {
-        return provider.dropDoublePlantShears(this);
+        return provider.dropDoublePlant(this);
     }
 
     @Override
