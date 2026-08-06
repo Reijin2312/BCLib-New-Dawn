d3990ae67668e9efba69bec840f9c6489d508a31
Redmi <terminator4444_4@mail.ru>
2026-08-06 20:24:24 +0500

    fixed logs stripping

--- src/main/java/org/betterx/bclib/blocks/BaseTrapdoorBlock.java
+++ src/main/java/org/betterx/bclib/blocks/BaseTrapdoorBlock.java
@@ -119,6 +119,7 @@ public abstract class BaseTrapdoorBlock extends TrapDoorBlock implements RenderL
         }
 
         generator.acceptBlockState(DatagenModelDispatch.dispatchWith(this, props));
+        generator.delegateItemModel(this, model);
     }
 
     public static class Wood extends BaseTrapdoorBlock implements BehaviourWood {
